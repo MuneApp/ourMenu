c8b34a7bdf0268e59bf2a4e426c17753e1bcae0b
hermitede1 <hermitede1@gmail.com>
2018-01-09 14:06:21 +0000

    local storage has been created for supper

--- src/pages/supper/supper.ts
+++ src/pages/supper/supper.ts
@@ -1,3 +1,4 @@
+import { Storage } from '@ionic/storage';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { IonicStorageModule } from '@ionic/storage';
@@ -21,12 +22,34 @@ Apettizer:string;
 Dessert:string;
 Main_Dish:string;
 Side_Dish:string;
+food:string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,private storage:Storage) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SupperPage');
+  }
+
+  save(){
+    let Main_Dish={
+      Food:this.food,
+    }
+    let numberOfDay =this.magicNumbers();
+    this.storage.set('DayName'+numberOfDay,Main_Dish).then(()=>{
+      alert("Datasaved");
+    }).catch((error)=>{
+      console.log(error);
+    });
+    this.food='';
+  }
+
+  magicNumbers(){
+    return Math.floor(Math.random()*1);
+  }
+
+  go(){
+    this.navCtrl.push("SupperPage");
   }
 
 }
